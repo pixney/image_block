16bb19c6a158eea9769633ec73de799a705ff83f
William <william@pixney.com>
2018-12-07 22:43:52 +0100

    Fields

--- migrations/2018_12_07_212356_pixney.extension.image_block__create_image_block_fields.php
+++ migrations/2018_12_07_212356_pixney.extension.image_block__create_image_block_fields.php
@@ -4,21 +4,12 @@ use Anomaly\Streams\Platform\Database\Migration\Migration;
 
 class PixneyExtensionImageBlockCreateImageBlockFields extends Migration
 {
-
     /**
      * The addon fields.
      *
      * @var array
      */
     protected $fields = [
-        'name' => 'anomaly.field_type.text',
-        'slug' => [
-            'type' => 'anomaly.field_type.slug',
-            'config' => [
-                'slugify' => 'name',
-                'type' => '_'
-            ],
-        ],
+        'image' => 'anomaly.field_type.image',
     ];
-
 }
