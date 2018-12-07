9aeaa899781e47ca24d1f7bb752c5cea56473b2e
William <william@pixney.com>
2018-12-07 22:31:12 +0100

    first commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Image Block',
+    'name'        => 'Image Block Extension',
+    'description' => ''
+];
---
+++ tests/Feature/ImageBlockExtensionTest.php
@@ -0,0 +1,10 @@
+<?php namespace Pixney\ImageBlockExtension\Test\Feature;
+
+class ImageBlockExtensionTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
