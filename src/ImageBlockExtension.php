3569425667a86581228cd65df929be3319e46565
William <william@pixney.com>
2019-01-07 00:25:53 +0100

    Using namespace

--- src/ImageBlockExtension.php
+++ src/ImageBlockExtension.php
@@ -9,12 +9,12 @@ class ImageBlockExtension extends BlockExtension
 {
     protected $provides = 'anomaly.module.blocks::block.image';
     protected $model    = BlockModel::class;
-    protected $wrapper  = 'pixney.extension.image_block::blocks/wrapper';
+    protected $wrapper  = 'theme::blocks/wrapper';
 
     public function getView()
     {
         // TODO : Either get active theme or config..
-        return 'pixney.theme.esharawater::blocks/image';
+        return 'themer::blocks/image';
 
         // if ($this->block->configuration('something_special')) {
         //     return 'my_company.extension.awesome_block::alternate_view';
