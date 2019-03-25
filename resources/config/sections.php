8d6ddca110e9f34136af2c5a2eb0f12fe35723ae
William <william@pixney.com>
2019-03-25 19:51:54 +0100

    Vertical Tabs

---
+++ resources/config/sections.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'block' => [
+        'fields' => []
+    ],
+    'options' => [
+        'stacked' => true,
+        'tabs'    => [
+            'image'     => [
+                'title'  => 'Image',
+                'fields' => [
+                    'entry_image',
+                ],
+            ],
+            'styling'     => [
+                'title'  => 'Styling',
+                'fields' => [
+                    'entry_classes',
+                ],
+            ]
+        ],
+    ]
+];
